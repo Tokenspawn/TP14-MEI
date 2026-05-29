7a4604c4b8b5879cfed60ddf06143cc754678522
micakir <micaakir@gmail.com>
2026-05-29 08:24:55 -0300

    funciona lo del contador

---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    int contador = 0;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Coleccionable"))
+        {
+            contador++;
+
+            Debug.Log("Objetos recolectados: " + contador);
+
+            Destroy(col.gameObject);
+        }
+    }
+}
