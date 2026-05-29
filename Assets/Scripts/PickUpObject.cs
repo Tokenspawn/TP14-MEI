68b79ee578eb92cb4c4c25dc18737157dcab4935
micakir <micaakir@gmail.com>
2026-05-29 08:39:22 -0300

    Clase 2: Mecánica de recolección funcional y modelo con materiales

--- Assets/Scripts/PickUpObject.cs
+++ Assets/Scripts/PickUpObject.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickUpObject : MonoBehaviour
 {
     int contador = 0;
+    public TextMeshProUGUI textoContador;
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.CompareTag("Coleccionable"))
         {
             contador++;
-
-            Debug.Log("Objetos recolectados: " + contador);
-
+            textoContador.text = "Objetos: " + contador;
             Destroy(col.gameObject);
         }
     }
